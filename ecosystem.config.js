793a9132a44f824e14e47ed6a7db2c0497551adb
kakao <ftilrftilr12@naver.com>
2018-04-05 21:55:48 +0900

    pm2 ecosystem edit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,11 @@
 module.exports = {
   apps :
       {
-        name      : 'pm2',
+        name      : 'inuclub',
         script    : 'index.js',
         watch     : true,
-        ignore_watch : ["logs", "public"],
+        ignore_watch : ["log", "public"],
         exec_mode : "cluster",
-        instances : 0,
-  
-        merge_logs : true,
-        log_date_format : "YY-MM-DD HH:mm:ss",
-        error_file : "./logs/err.log",
-        out_file : "./logs/out.log"
+        instances : 0
       }
   };
